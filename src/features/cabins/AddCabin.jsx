6d71f1131a0582e7d7dc8b8ee36af174c53b8b07
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2026-03-08 02:08:15 +0500

    feat(cabins): add cabin table modal button in AddCabin

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,36 +1,36 @@
+import styled from "styled-components";
 import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
+import CabinTable from "./CabinTable";
 import CreateCabinForm from "./CreateCabinForm";
+
+const Actions = styled.div`
+  display: grid;
+  gap: 1.2rem;
+`;
 
 function AddCabin() {
   return (
     <Modal>
-      <Modal.Open opens="cabin-form">
-        <Button style={{ width: "100%" }}>Add new cabin</Button>
-      </Modal.Open>
+      <Actions>
+        <Modal.Open opens="cabin-form">
+          <Button style={{ width: "100%" }}>Add new cabin</Button>
+        </Modal.Open>
+        <Modal.Open opens="cabin-table">
+          <Button style={{ width: "100%" }} variation="secondary">
+            Show table
+          </Button>
+        </Modal.Open>
+      </Actions>
+
       <Modal.Window name="cabin-form">
         <CreateCabinForm />
+      </Modal.Window>
+      <Modal.Window name="cabin-table">
+        <CabinTable />
       </Modal.Window>
     </Modal>
   );
 }
-
-// function AddCabin() {
-//   const [isOpenModal, setIsOpenModal] = useState(false);
-
-//   return (
-//     <>
-//       <Button style={{ width: "100%" }} onClick={() => setIsOpenModal(true)}>
-//         Add new cabin
-//       </Button>
-
-//       {isOpenModal && (
-//         <Modal onClose={() => setIsOpenModal(false)}>
-//           <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
-//         </Modal>
-//       )}
-//     </>
-//   );
-// }
 
 export default AddCabin;
